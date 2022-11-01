22fdfd9c71e97eb8c9655a8bb6b76e89c59e9102
Joao Paulo Prata Costa <joaoppcosta@hotmail.com.br>
2022-11-01 18:40:22 -0300

    Urna simulada

---
+++ send.js
@@ -0,0 +1,55 @@
+// Load the AWS SDK for Node.js
+var AWS = require('aws-sdk');
+// Set the region 
+AWS.config.loadFromPath(__dirname + '/config.json');
+// Create an SQS service object
+var sqs = new AWS.SQS({ apiVersion: '2012-11-05' });
+function sleep(ms) {
+    return new Promise((resolve) => {
+        setTimeout(resolve, ms);
+    });
+}
+
+async function main() {
+    console.log("Iniciando apuração");
+    while (true) {
+
+        var candidato1 = Math.floor(Math.random() * 100)
+        var candidato2 = Math.floor(Math.random() * 80)
+        var nulos = Math.floor(Math.random() * 15)
+        var brancos = Math.floor(Math.random() * 20)
+        var params = {
+            DelaySeconds: 0,
+            MessageAttributes: {
+                "Candidato1": {
+                    DataType: "Number",
+                    StringValue: candidato1.toString()
+                },
+                "Candidato2": {
+                    DataType: "Number",
+                    StringValue: candidato2.toString()
+                },
+                "VotosNulos": {
+                    DataType: "Number",
+                    StringValue: nulos.toString()
+                },
+                "Brancos": {
+                    DataType: "Number",
+                    StringValue: brancos.toString()
+                }
+            },
+            MessageBody: "Apuração dos votos",
+            QueueUrl: "https://sqs.us-east-1.amazonaws.com/200416781616/SQS_QUEUE_NAME"
+        };
+
+        sqs.sendMessage(params, function (err, data) {
+            if (err) {
+                console.log("Error", err);
+            } else {
+                console.log("Success", data.MessageId);
+            }
+        });
+        await sleep(10000);
+    }
+}
+main()
